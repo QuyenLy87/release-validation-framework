83317639ca9cc09eb64c55b19b833d0839fb372b
HMVNLTP015\QC <quyen.ly@huntermacdonald.com>
2017-06-22 17:02:25 +0700

    Implement RQI-60_Failures No7

---
+++ importer/src/main/resources/scripts/component-centric/component-centric-snapshot-refset-descriptor-referencedcomponentid-attributeorder-pair-is-unique.sql
@@ -0,0 +1,21 @@
+/********************************************************************************
+	component-centric-snapshot-refset-descriptor-referencedcomponentid-attributeorder-pair-is-unique
+
+	Assertion:
+	referencedComponentId + attributeOrder pair is unique in REFSET DESCRIPTOR SNAPSHOT
+
+********************************************************************************/
+	insert into qa_result (runid, assertionuuid, concept_id, details)
+	select
+		<RUNID>,
+		'<ASSERTIONUUID>',
+		a.moduleid,
+		concat('Refset: id=',a.id,' with ReferencedComponentId = ',a.referencedcomponentid,' + AttributeOrder = ',a.attributeorder,'  is not unique pair in REFSET DESCRIPTOR SNAPSHOT')
+	from curr_refsetDescriptor_s a
+    where (a.referencedcomponentid,a.attributeorder) in (
+													select b.referencedcomponentid,b.attributeorder
+													from curr_refsetDescriptor_s b
+													group by b.referencedcomponentid,b.attributeorder
+													having count(*) > 1
+													);
+	commit;
