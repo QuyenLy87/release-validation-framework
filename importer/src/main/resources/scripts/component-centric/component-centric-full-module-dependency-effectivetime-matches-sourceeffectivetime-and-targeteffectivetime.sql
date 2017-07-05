a720be031a088014b20caa83a183fa4753a4925d
HMVNLTP015\QC <quyen.ly@huntermacdonald.com>
2017-07-05 10:49:29 +0700

    Combine script for RQI-60/Failures No35 into No34

--- importer/src/main/resources/scripts/component-centric/component-centric-full-module-dependency-effectivetime-matches-sourceeffectivetime-and-targeteffectivetime.sql
+++ importer/src/main/resources/scripts/component-centric/component-centric-full-module-dependency-effectivetime-matches-sourceeffectivetime-and-targeteffectivetime.sql
@@ -3,7 +3,7 @@
 
 	Assertion:
 	For each record in Module Dependency Full file, the effectiveTime always matches the sourceEffectiveTime and targetEffectiveTime
-	(for International Edition only)
+	(International Edition or all Derivative Packages)
 
 ********************************************************************************/
 	insert into qa_result (runid, assertionuuid, concept_id, details)
@@ -15,5 +15,11 @@
 	from curr_moduleDependency_f a, package_info b
 	where a.effectivetime <> a.sourceeffectivetime
     and a.sourceeffectivetime = a.targeteffectivetime
-    and b.releaseedition = 'INT';
+    and (
+    b.releaseedition = 'INT'
+    or b.releaseedition = 'SE'
+    or b.releaseedition = 'DK'
+    or b.releaseedition = 'GPFP'
+    or b.releaseedition = 'ICNP'
+    );
 	commit;
