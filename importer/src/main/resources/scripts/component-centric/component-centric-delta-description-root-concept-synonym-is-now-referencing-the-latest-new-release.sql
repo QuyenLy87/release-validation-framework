0e6f4d0d6652697e36dd89e5e64d58f55113758f
HMVNLTP015\QC <quyen.ly@huntermacdonald.com>
2017-07-07 15:07:41 +0700

    Implement RQI-60/Failures No17

--- importer/src/main/resources/scripts/component-centric/component-centric-delta-description-root-concept-synonym-is-now-referencing-the-latest-new-release.sql
+++ importer/src/main/resources/scripts/component-centric/component-centric-delta-description-root-concept-synonym-is-now-referencing-the-latest-new-release.sql
@@ -13,7 +13,7 @@
             result.expression
             from
             (select a.id,
-                     concat('The root concept id = ',a.id,' synonym is not now referencing the latest new release') expression
+                     concat('Concept id = ',a.id,', root concept synonym is not referencing the latest new release') expression
                      from curr_description_d a, package_info b
                      where a.conceptid= '138875005'
                      and a.typeid = '900000000000013009'
@@ -30,7 +30,7 @@
         result.expression
         from
         (select a.id,
-                  concat('The root concept id = ',a.id,' synonym is now referencing the latest new release but is in inactivate state') expression
+                  concat('Concept id = ',a.id,', root concept synonym is now referencing the latest new release but is in inactivate state') expression
                   from curr_description_d a, package_info b
                   where a.conceptid= '138875005'
                   and a.typeid = '900000000000013009'
@@ -47,7 +47,7 @@
             result.expression
             from
             (select a.id,
-                      concat('The root concept id = ',a.id,' synonym is not valid') expression
+                      concat('Concept id = ',a.id,', root concept synonym is not valid') expression
                       from curr_description_d a, package_info b
                       where a.conceptid= '138875005'
                       and a.typeid = '900000000000013009'
